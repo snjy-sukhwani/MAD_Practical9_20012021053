1e90d860dd7a7bdf71ffc7d036f204993d931b23
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-11-03 14:53:21 +0530

    Updated : Added Kotlin Code for Working of Application

--- app/src/main/java/com/example/mad_practical9_20012021053/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical9_20012021053/MainActivity.kt
@@ -1,11 +1,123 @@
 package com.example.mad_practical9_20012021053
 
+import android.Manifest
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
+import android.telephony.SmsManager
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.mad_practical9_20012021053.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private val SMS_PERMISSION_CODE = 100
+    private lateinit var lv: ListView
+    private lateinit var al: ArrayList<SMSView>
+    private lateinit var smsReceiver:SMSBroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lv = binding.listView
+        al = ArrayList()
+
+        if(checkRequestPermission()){
+            loadSMSInbox()
+        }
+
+        smsReceiver = SMSBroadcastReceiver()
+        registerReceiver(smsReceiver, IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION))
+        smsReceiver.setListner(ListenerImplement())
+        binding.sendButton.setOnClickListener{
+            val phone = binding.phoneNo.text.toString()
+            val msg = binding.msg.text.toString()
+            if(phone == ""){
+                Toast.makeText(this,"Please Enter Mobile Number !",Toast.LENGTH_SHORT).show()
+            }
+            else if(msg == ""){
+                Toast.makeText(this,"Please Enter Message !",Toast.LENGTH_SHORT).show()
+            }
+            else if(phone.length!=10){
+                Toast.makeText(this,"Please Enter Valid Phone Number !",Toast.LENGTH_SHORT).show()
+            }
+            else if(msg.length>=700){
+                Toast.makeText(this,"Message Length is too Long !",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                sendSms(phone,msg)
+            }
+        }
+
+    }
+
+    private fun sendSms(sPhoneNo: String?,sMsg: String?){
+        if(!checkRequestPermission()){
+            return
+        }
+        val smsmanager = SmsManager.getDefault()
+        if(smsmanager != null){
+            smsmanager.sendTextMessage(sPhoneNo,null,sMsg,null,null)
+        }
+    }
+
+    inner class ListenerImplement:SMSBroadcastReceiver.Listner{
+        override fun onTextReceived(sPhoneNo: String?, sMsg: String?) {
+            val builder : AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+            builder.setTitle("New SMS Received")
+            builder.setMessage("$sPhoneNo\n$sMsg")
+            builder.setCancelable(true)
+            builder.show()
+            loadSMSInbox()
+        }
+    }
+
+    private val isSMSReadPermission: Boolean
+        get() = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
+    private val isSMSWritePermission: Boolean
+        get() = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestSMSPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
+            // You may display a non-blocking explanation here, read more in the documentation:
+            // https://developer.android.com/training/permissions/requesting.html
+        }
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS,
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.RECEIVE_SMS),
+            SMS_PERMISSION_CODE)
+    }
+
+    private fun checkRequestPermission(): Boolean {
+        return if (!isSMSReadPermission || !isSMSWritePermission) {
+            requestSMSPermission()
+            false
+        } else true
+    }
+
+    private fun loadSMSInbox() {
+        if (!checkRequestPermission()) return
+        val uriSMS = Uri.parse("content://sms/inbox")
+        val c = contentResolver.query(uriSMS, null, null, null, null)
+        al.clear()
+        while (c!!.moveToNext()) {
+            al.add(SMSView(c.getString(2),c.getString(12)))
+        }
+        lv.adapter = SMSViewAdapter(this,al)
+
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(smsReceiver)
+        super.onDestroy()
     }
 }
